4885063cbc892272b68bb9f4297fe8e6d7c63e3b
Adam S. Cohen <adamcohen732@protonmail.com>
2024-12-13 09:23:45 -1000

    Add files via upload
    Reran sql query to include adoption and paternity *filings* for FY24

--- FY24_filings/pfp_analysis_2024-11-18.R
+++ FY24_filings/pfp_analysis_2024-11-18.R
@@ -6,13 +6,19 @@ library(dplyr)
 
 # import data -------------------------------------------------------------
 
-pfp <- read_excel("pfp_extract_2024-11-18_1401.xlsx")
+pfp <- read_excel("pfp_extract_2024-12-13_0917.xlsx")
 
-filings_circuit <- pfp %>% 
+filings <- pfp %>% 
   mutate(CASE_TYPE_recode = case_when(CASE_TYPE %in% c("TC","TI","TP") ~ "traffic_filings", 
                                       CASE_TYPE == "CV" ~ "civil_action_filings",
                                       CASE_TYPE == "PC" ~ "criminal_action_filings",
-                                      CASE_TYPE == "DV" ~ "marital_action_filings"),
-         CASE_TYPE_recode = factor(CASE_TYPE_recode, levels = c("civil_action_filings","criminal_action_filings", 
-                                                                 "marital_action_filings", "traffic_filings"))) %>% 
-  count(COURT, CASE_TYPE_recode, name = "frequency (n)")
+                                      CASE_TYPE == "DV" ~ "marital_action_filings",
+                                      CASE_TYPE == "AN" ~ "adoption_filings",
+                                      CASE_TYPE == "PA" ~ "paternity_filings"
+                                      ),
+         case_type = factor(CASE_TYPE_recode, levels = c("civil_action_filings","criminal_action_filings", 
+                                                         "marital_action_filings", "adoption_filings", 
+                                                         "paternity_filings", "traffic_filings"))) %>% 
+  count(circuit = COURT, case_type, name = "frequency (n)")
+
+write.csv(filings, paste0("pfp_results_", Sys.Date(),".csv"), row.names = F)
